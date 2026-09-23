6b2a230d893746134a14d7d6f7854badf349182b
Lucas <lucasjbookey@gmail.com>
2026-09-23 16:52:27 +0100

    sending message on CAN

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # fsai-test-car
 
 ## Setup
-Install libsub 1.0 binaries to be able to use the pyusb library.
+Install libsub 1.0 binaries to be able to use the pyusb library, store dll in System32 folder
+
--- test.py
+++ test.py
@@ -7,24 +7,49 @@ import time
 #     print(i, d)
 
 
-# bus = can.interface.Bus(channel='0', bustype='gs_usb', bitrate=500000)
-# msg = can.Message(arbitration_id=0x123, data=[1, 2, 3, 4], is_extended_id=False)
-# bus.send(msg)
-# print("Sent!")
+print("Opening CAN device...")
+
+bus = can.Bus(
+    interface="gs_usb",
+    channel=0,
+    bitrate=500000
+)
+
+print("CAN device opened successfully")
+print("Sending message...")
+
+msg = can.Message(
+    arbitration_id=0x123,
+    data=[1, 2, 3, 4],
+    is_extended_id=False
+)
+
+try:
+    bus.send(msg, timeout=2)
+    print("SUCCESS: message sent")
+except Exception as e:
+    print("SEND ERROR:", repr(e))
+finally:
+    try:
+        bus.shutdown()
+    except Exception:
+        pass
+
+print("Done")
 
 
-bus1 = can.interface.Bus('test', interface='virtual')
-bus2 = can.interface.Bus('test', interface='virtual')
+# bus1 = can.interface.Bus('test', interface='virtual')
+# bus2 = can.interface.Bus('test', interface='virtual')
 
-msg1 = can.Message(arbitration_id=0xabcde, data=[1,2,3])
-bus1.send(msg1)
-msg2 = bus2.recv()
+# msg1 = can.Message(arbitration_id=0xabcde, data=[1,2,3])
+# bus1.send(msg1)
+# msg2 = bus2.recv()
 
-#assert msg1 == msg2
-assert msg1.arbitration_id == msg2.arbitration_id
-assert msg1.data == msg2.data
-assert msg1.timestamp != msg2.timestamp
+# #assert msg1 == msg2
+# assert msg1.arbitration_id == msg2.arbitration_id
+# assert msg1.data == msg2.data
+# assert msg1.timestamp != msg2.timestamp
 
 
-bus1.shutdown()
-bus2.shutdown()
+# bus1.shutdown()
+# bus2.shutdown()
